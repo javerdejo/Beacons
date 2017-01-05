d9eaa6ec67a6f092e687620294fc6195a7ab004a
javerdejo <javier_arellano_verdejo@hotmail.com>
2017-01-05 14:02:59 +0100

    Se agregó splash de inicio a la aplicación

---
+++ app/src/main/java/com/example/umacamp/SplashAppActivity.java
@@ -0,0 +1,39 @@
+package com.example.umacamp;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_app);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        SplashAppActivity.this, MainApp.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+    }
+}
